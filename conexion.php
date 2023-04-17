c88147495a7ffecfec0ab88733113f433d2b0156
xt4568 <xt4568@rimac.com.pe>
2023-04-16 21:28:01 -0500

    conexion

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set("America/Lima");
 //ini_set("session.cookie_lifetime","86400");
-$dbhost     = '3.237.52.136';	//host del Mysql 
+$dbhost     = '3.237.52.136:3308';	//host del Mysql 
 $dbUsuario  = 'root';	//En este caso el servidor no tiene valor para usuario para acceder a la base
 $dbpassword = 'SharedABC2023';	//Aqui tambien no hay un valor especifico
 $db         ='farmasi2_bfamifarma';		// Nombre de la Base Datos
@@ -13,7 +13,7 @@ $conexion = mysqli_connect($dbhost, $dbUsuario, $dbpassword, $db);
 
 
 /////////////////////////////////////////////////SEGUNDA CONEXION///////////////////////////////////////////////////////////////////////////////////////
-$dbhost2     = '3.237.52.136';	//host del Mysql 
+$dbhost2     = '3.237.52.136:3308';	//host del Mysql 
 $dbUsuario2  = 'root';	//En este caso el servidor no tiene valor para usuario para acceder a la base
 $dbpassword2 = 'SharedABC2023';	//Aqui tambien no hay un valor especifico
 $db2         ='farmasi2_bfamifarma';		// Nombre de la Base Datos
